a74e09b1a7b994d2478545e3bf72b1c76eaef8c3
George <sanchez28112001@gmail.com>
2022-01-01 22:03:20 -0500

    finished FirstActivity and added QuestionsActivity

---
+++ app/src/main/java/com/sanv/myquizapp/Constants.kt
@@ -0,0 +1,12 @@
+package com.sanv.myquizapp
+
+object Constants {
+
+    /*fun getQuestions():ArrayList<Question>{
+        val questionList = ArrayList<Question>()
+
+        val que1 = Question(
+            1, "What country does this flag belong to?",
+        )
+    }*/
+}
